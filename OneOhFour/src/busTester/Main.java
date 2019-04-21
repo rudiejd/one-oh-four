d49df43c7e673f627c95d2c697cfc309c36fb6a3
jd <jd@DESKTOP-FDH100D>
2019-04-21 15:33:16 -0400

    update stuff

--- OneOhFour/src/busTester/Main.java
+++ OneOhFour/src/busTester/Main.java
@@ -1,5 +1,7 @@
 package busTester;
 
 public class Main {
-
+	public static void main(String[] args) {
+		System.out.println("hi there");
+	}
 }
--- OneOhFour/src/busTester/Tester.java
+++ OneOhFour/src/busTester/Tester.java
@@ -1,5 +1,4 @@
 package busTester;
 
 public class Tester {
-
 }
