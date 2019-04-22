a609ced558306b0fb1523445a77bbb2eec3894af
jd <jd@DESKTOP-FDH100D>
2019-04-21 23:13:44 -0400

    update to get it to the point where i can open the first lab

--- OneOhFour/src/busTester/Main.java
+++ OneOhFour/src/busTester/Main.java
@@ -1,7 +1,29 @@
 package busTester;
 
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("hi there friendo");
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the path to your lab folder: ");
+		File folder = new File(buff.readLine());
+		System.out.println("Enter the lab number you want to test: ");
+		int labNumber = Integer.parseInt(buff.readLine());
+		
+		Tester test = new Tester(labNumber, folder);
+		
 	}
+	
+
+	
+	
+	
 }
--- OneOhFour/src/busTester/Tester.java
+++ OneOhFour/src/busTester/Tester.java
@@ -1,4 +1,75 @@
 package busTester;
 
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Tester {
+	private int labNumber;
+	private static WebDriver driver;
+	private File folder;
+	
+	public Tester(int labNumber, File folder) {
+		this.labNumber = labNumber;
+		this.folder = folder;
+		System.out.println(this.test());
+	}
+
+	
+	public int[] test() {
+		switch (this.labNumber) {
+			case 1:
+				return labOneTest();
+			case 2:
+				return labTwoTest();
+			case 3:
+				return labThreeTest();
+			case 4:
+				return labFourTest();
+			default:
+				return new int[0];
+		}
+	}
+	
+	private int[] labOneTest() {
+		
+		File[] labs = this.folder.listFiles();
+		int[] res = new int[labs.length];
+		for (int i = 0; i < labs.length; i++) {
+			int points = 0;
+			initWeb();
+			driver.get(labs[i].getPath());
+			
+			res[i] = points;
+		}
+		
+		return res;
+	}
+	
+	private int[] labTwoTest() {
+		int[] res = new int[0];
+		return res;
+	}
+	
+	private int[] labThreeTest() {
+		int[] res = new int[0];
+		return res;
+	}
+	
+	private int[] labFourTest() {
+		int[] res = new int[0];
+		return res;
+	}
+	
+	
+	public static void initWeb() {
+		String path = System.getProperty("user.home");
+		System.setProperty("webdriver.chrome.driver", path + "\\Driver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+	}
+	
+	
+	
 }
